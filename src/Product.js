748d4bf04a4445d39930eacb7614b76cab5678b8
ChoudhryAwais <38686813+ChoudhryAwais@users.noreply.github.com>
2020-08-09 22:50:25 +0500

    update(Emoji)
    aria=list of string

--- src/Product.js
+++ src/Product.js
@@ -31,7 +31,7 @@ function Product({id,title,image,price,rating}) {
                         Array(rating)
                         .fill()
                         .map((_) => (
-                            <span role="img" aria-label="true" aria-labelledby="true">⭐️</span>
+                            <span role="img" aria-label="start" aria-labelledby="emoji emojilabel">⭐️</span>
                         ))
                     }
                 </div>
